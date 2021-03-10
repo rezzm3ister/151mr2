4c0bec9decd34369961db1912950cdcbeab85f23
Luis Alvarez <mlpa16.2@gmail.com>
2021-03-10 17:13:27 +0800

    introductory commit

---
+++ functions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include "functions.h"
+using namespace std;
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "functions.h"
+using namespace std;
+
+int main()
+{
+  cout<<"hello world";
+}
